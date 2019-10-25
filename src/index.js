e0df7125788f3285fcfecc7b30e711dc1bea5a84
ThalesLoreto <thalesmulti3@gmail.com>
2019-10-25 16:56:55 -0300

    setting up navigation with stack && fix statusBar

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
+
 export default function App() {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#FFF',
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
-  });
-
-  console.tron.log('Iha');
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native App</Text>
-      <Text style={styles.welcome}>Texto abaixo s</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 }
